9f2de2865136eeb4cc67fc1ff01eeea9b0e16e91
PhiGei2000 <Philipp-Geil@outlook.com>
2023-02-18 17:49:14 +0100

    Fix type errors and add production environment

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,6 +1,10 @@
 import Header from "./header";
 
-export default function Layout({ children }) {
+export type LayoutProps = {
+    children: string | JSX.Element | JSX.Element[] | (() => JSX.Element)
+};
+
+export default function Layout({ children }: LayoutProps) {
     return (
         <>
             <Header />
